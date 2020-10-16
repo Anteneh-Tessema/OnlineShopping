0c823a8cf2655571e42346b8aa61968dc80e3ae0
Anteneh <attessema@mum.edu>
2020-10-16 17:57:16 -0400

    Testing the branch

--- src/main/java/com/cs490/onlineshipping/OnlineshippingApplication.java
+++ src/main/java/com/cs490/onlineshipping/OnlineshippingApplication.java
@@ -8,6 +8,8 @@ public class OnlineshippingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineshippingApplication.class, args);
+        System.out.println("good code");
+
     }
 
 }
