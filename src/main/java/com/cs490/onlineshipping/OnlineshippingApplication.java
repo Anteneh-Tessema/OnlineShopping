240b4a222e93386a8c757b06e8a8663862eef06a
Anteneh <attessema@mum.edu>
2020-10-16 15:41:06 -0400

    Initial commit-Anteneh

---
+++ src/main/java/com/cs490/onlineshipping/OnlineshippingApplication.java
@@ -0,0 +1,13 @@
+package com.cs490.onlineshipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshippingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineshippingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs490/onlineshipping/OnlineshippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs490.onlineshipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshippingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
