516fec43b0572949e20f179e1c1db7777621aade
Anteneh <attessema@mum.edu>
2020-10-17 23:16:34 -0400

    Init

--- src/main/java/com/cs490/onlineshopping/OnlineshoppingApplication.java
+++ src/main/java/com/cs490/onlineshopping/OnlineshoppingApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineshoppingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineshoppingApplication.class, args);
-
-
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshoppingApplication.class, args);
+	}
 
 }
