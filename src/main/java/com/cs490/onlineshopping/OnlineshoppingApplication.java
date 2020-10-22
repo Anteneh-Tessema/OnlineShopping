1864f6a401d8aecb37f118738aca6e09af3e4700
Anteneh <attessema@mum.edu>
2020-10-22 18:44:34 -0400

    Creat Admin Service Classes
    Signed-off-by: Anteneh <attessema@mum.edu>

--- src/main/java/com/cs490/onlineshopping/OnlineshoppingApplication.java
+++ src/main/java/com/cs490/onlineshopping/OnlineshoppingApplication.java
@@ -8,7 +8,7 @@ public class OnlineshoppingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineshoppingApplication.class, args);
-		System.out.println("This is test!!");
+		System.out.println("This is Anteneh Test!!");
 	}
 
 }
