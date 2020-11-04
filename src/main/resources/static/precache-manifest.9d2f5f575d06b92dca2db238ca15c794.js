700dadfcb20066690acbe7ab2a28450143831e97
Amit Bhattarai <amit.bhattarai46@gmail.com>
2020-11-04 14:22:52 -0800

    Added report module

---
+++ src/main/resources/static/precache-manifest.9d2f5f575d06b92dca2db238ca15c794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0483e4f23150cf1151607881cb86b4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bb5029c2d37b8e487df",
+    "url": "/static/css/main.53f63260.chunk.css"
+  },
+  {
+    "revision": "497bf95faaec6a8f30bd",
+    "url": "/static/js/2.b5e714a2.chunk.js"
+  },
+  {
+    "revision": "279f4500c026e6dd4fed378fd3e233ed",
+    "url": "/static/js/2.b5e714a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb5029c2d37b8e487df",
+    "url": "/static/js/main.019ae7fe.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
