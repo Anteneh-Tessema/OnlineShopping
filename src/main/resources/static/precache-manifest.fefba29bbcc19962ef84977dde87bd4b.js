1573e51e87b3b75fce1a2b540b239397a7a96b88
Amit Bhattarai <amit.bhattarai46@gmail.com>
2020-11-04 20:33:31 -0800

    Changes to reports module

---
+++ src/main/resources/static/precache-manifest.fefba29bbcc19962ef84977dde87bd4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2274832ee97b68689a7fed9e04e40be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83db0ef5d9bcb9becb48",
+    "url": "/static/css/main.53f63260.chunk.css"
+  },
+  {
+    "revision": "dc7afd529891b49429f1",
+    "url": "/static/js/2.49387a86.chunk.js"
+  },
+  {
+    "revision": "279f4500c026e6dd4fed378fd3e233ed",
+    "url": "/static/js/2.49387a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83db0ef5d9bcb9becb48",
+    "url": "/static/js/main.a5e70397.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
